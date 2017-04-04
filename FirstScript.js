7ff0fc7dee93010c28392732036b96d6660c76dc
AlexanderBeaven <Beaven1@live.missouristate.edu>
2017-04-04 18:21:51 -0500

    ghghg

--- FirstScript.js
+++ FirstScript.js
@@ -10,45 +10,55 @@ function checkForm() {
     {
         fieldsCount++;
         document.getElementById("fN").style.backgroundColor = "#c2f5c2";
+        document.getElementById("cM").style.borderColor = "#7AE7C7";
     }
     else {
         document.getElementById("fN").style.backgroundColor = "#ffd0d0";
+        document.getElementById("cM").style.borderColor = "#EF959D";
     }
 
     if (lName != "")
     {
         fieldsCount++;
         document.getElementById("lN").style.backgroundColor = "#c2f5c2";
+        document.getElementById("cM").style.borderColor = "#7AE7C7";
     }
     else {
         document.getElementById("lN").style.backgroundColor = "#ffd0d0";
+        document.getElementById("cM").style.borderColor = "#EF959D";
     }
 
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(eMail))
     {
         fieldsCount++;
         document.getElementById("eM").style.backgroundColor = "#c2f5c2";
+        document.getElementById("cM").style.borderColor = "#7AE7C7";
     }
     else {
         document.getElementById("eM").style.backgroundColor = "#ffd0d0";
+        document.getElementById("cM").style.borderColor = "#EF959D";
     }
 
     if (/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(phone))   
     {
         fieldsCount++;
         document.getElementById("pN").style.backgroundColor = "#c2f5c2";
+        document.getElementById("cM").style.borderColor = "#7AE7C7";
     }
     else {
         document.getElementById("pN").style.backgroundColor = "#ffd0d0";
+        document.getElementById("cM").style.borderColor = "#EF959D";
     }
 
     if (comment != "")
     {
         fieldsCount++;
         document.getElementById("cM").style.backgroundColor = "#c2f5c2";
+        document.getElementById("cM").style.borderColor = "#7AE7C7";
     }
     else {
         document.getElementById("cM").style.backgroundColor = "#ffd0d0";
+        document.getElementById("cM").style.borderColor = "#EF959D";
     }
 
     if (fieldsCount < 5) {
