7bdaa4e6aa1ce840a6508c44230b07492d95e67a
AlexanderBeaven <Beaven1@live.missouristate.edu>
2017-04-04 18:34:38 -0500

    cxcxcxx

--- FirstScript.js
+++ FirstScript.js
@@ -10,44 +10,44 @@ function checkForm() {
     {
         fieldsCount++;
         document.getElementById("fN").style.backgroundColor = "#c2f5c2";
-        document.getElementById("cM").style.borderColor = "#7AE7C7";
+        document.getElementById("fN").style.borderColor = "#7AE7C7";
     }
     else {
         document.getElementById("fN").style.backgroundColor = "#ffd0d0";
-        document.getElementById("cM").style.borderColor = "#EF959D";
+        document.getElementById("fN").style.borderColor = "#EF959D";
     }
 
     if (lName != "")
     {
         fieldsCount++;
         document.getElementById("lN").style.backgroundColor = "#c2f5c2";
-        document.getElementById("cM").style.borderColor = "#7AE7C7";
+        document.getElementById("lN").style.borderColor = "#7AE7C7";
     }
     else {
         document.getElementById("lN").style.backgroundColor = "#ffd0d0";
-        document.getElementById("cM").style.borderColor = "#EF959D";
+        document.getElementById("lN").style.borderColor = "#EF959D";
     }
 
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(eMail))
     {
         fieldsCount++;
         document.getElementById("eM").style.backgroundColor = "#c2f5c2";
-        document.getElementById("cM").style.borderColor = "#7AE7C7";
+        document.getElementById("eM").style.borderColor = "#7AE7C7";
     }
     else {
         document.getElementById("eM").style.backgroundColor = "#ffd0d0";
-        document.getElementById("cM").style.borderColor = "#EF959D";
+        document.getElementById("eM").style.borderColor = "#EF959D";
     }
 
     if (/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(phone))   
     {
         fieldsCount++;
         document.getElementById("pN").style.backgroundColor = "#c2f5c2";
-        document.getElementById("cM").style.borderColor = "#7AE7C7";
+        document.getElementById("pN").style.borderColor = "#7AE7C7";
     }
     else {
         document.getElementById("pN").style.backgroundColor = "#ffd0d0";
-        document.getElementById("cM").style.borderColor = "#EF959D";
+        document.getElementById("pN").style.borderColor = "#EF959D";
     }
 
     if (comment != "")
