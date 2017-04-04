ff2aee47037d47805334651d6e25c9f3ebd1d38d
AlexanderBeaven <Beaven1@live.missouristate.edu>
2017-04-04 15:47:07 -0500

    ...

--- FirstScript.js
+++ FirstScript.js
@@ -2,29 +2,48 @@ function checkForm() {
     var fieldsCount = 0;
     var fName = document.forms["contactForm"]["firstName"].value;
     var lName = document.forms["contactForm"]["lastName"].value;
-    var eMail = document.forms["contactForm"]["firstName"].value;
-    var phone = document.forms["contactForm"]["lastName"].value;
-    var comment = document.forms["contactForm"]["lastName"].value;
+    var eMail = document.forms["contactForm"]["email"].value;
+    var phone = document.forms["contactForm"]["phone"].value;
+    var comment = document.forms["contactForm"]["comment"].value;
 
-    if (fName != "") {
-
+    if (fName != "")
+    {
         fieldsCount++;
     }
-    if (lName != "") {
+    else {
+        document.getElementById("fN").style.color = "#EF959D";
+    }
 
+    if (lName != "")
+    {
         fieldsCount++;
     }
-    if (eMail != "") {
+    else {
+        document.getElementById("lN").style.color = "#EF959D";
+    }
 
+    if (eMail != "")
+    {
         fieldsCount++;
     }
-    if (phone != "") {
+    else {
+        document.getElementById("eM").style.color = "#EF959D";
+    }
 
+    if (phone != "")
+    {
+       fieldsCount++;
+    }
+    else {
+        document.getElementById("pN").style.color = "#EF959D";
+    }
+
+    if (comment != "")
+    {
         fieldsCount++;
     }
-    if (comment != "") {
-
-        fieldsCount++;
+    else {
+        document.getElementById("cM").style.color = "#EF959D";
     }
 
     if (fieldsCount < 5)
