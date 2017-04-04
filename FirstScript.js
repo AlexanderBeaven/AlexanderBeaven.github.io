ed96a7e375ebb5789547fb796d48e327b8d27206
AlexanderBeaven <Beaven1@live.missouristate.edu>
2017-04-04 15:18:08 -0500

    ...

--- FirstScript.js
+++ FirstScript.js
@@ -1,2 +1,37 @@
+function checkForm() {
+    var fieldsCount = 0;
+    var fName = document.forms["contactForm"]["firstName"].value;
+    var lName = document.forms["contactForm"]["lastName"].value;
+    var eMail = document.forms["contactForm"]["firstName"].value;
+    var phone = document.forms["contactForm"]["lastName"].value;
+    var comment = document.forms["contactForm"]["lastName"].value;
 
+    if (fName != "") {
+
+        fieldsCount++;
+    }
+    if (lName != "") {
+
+        fieldsCount++;
+    }
+    if (eMail != "") {
+
+        fieldsCount++;
+    }
+    if (phone != "") {
+
+        fieldsCount++;
+    }
+    if (comment != "") {
+
+        fieldsCount++;
+    }
+
+    if (fieldsCount < 5)
+    {
+        alert("One or more fields are invalid.");
+    }
+
+
+}
 
