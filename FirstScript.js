3aa4aec61651dc35c90e0de00f86ffa7939f4460
AlexanderBeaven <Beaven1@live.missouristate.edu>
2017-04-10 16:19:26 -0500

    fdfddf

--- FirstScript.js
+++ FirstScript.js
@@ -68,7 +68,26 @@ function checkForm() {
         document.getElementById("sub").setAttribute('type', 'submit');
         alert("The form has been submitted. Thank you.");
     }
+}
 
+/* When the user clicks on the button,
+        toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn2')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content2");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
 }
 
