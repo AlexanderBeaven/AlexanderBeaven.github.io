564cbdd77657b2e309d2f1b245812933de79a93d
AlexanderBeaven <Beaven1@live.missouristate.edu>
2017-04-30 13:25:40 -0500

    v.001

--- SpaceRaceScript.js
+++ SpaceRaceScript.js
@@ -1,27 +1,52 @@
 var running = false;
+var reset = true;
 var ship1Left = 0;
 var ship2Left = 0;
-var finish = 97;
+var ship3Left = 0;
+var ship4Left = 0;
+var ship5Left = 0;
+
+var finish = screen.width;
 
 function GameStarted()
 {
-    if (!running)
+    if (!running && reset)
     {
-        ship1Left = 0;
-        ship2Left = 0;
+        reset = false;
         running = true;
-        document.getElementById("ship1").style.left = "0%";
-        document.getElementById("ship2").style.left = "0%";
         document.getElementById("message").style.visibility = "hidden";
         document.getElementById("startButton").style.backgroundImage = "url(srimages/GreenLight.png)";
         loop();
     }
 }
 
-function loop() {
-    if (running) {
-        var rand = Math.round(Math.random() * (200 - 100)) + 100;
-        setTimeout(function () {
+function Reset()
+{
+    if (!reset)
+    {
+    reset = true;
+    ship1Left = 0;
+    ship2Left = 0;
+    ship3Left = 0;
+    ship4Left = 0;
+    ship5Left = 0;
+    document.getElementById("ship1").style.left = "0%";
+    document.getElementById("ship2").style.left = "0%";
+    document.getElementById("ship3").style.left = "0%";
+    document.getElementById("ship4").style.left = "0%";
+    document.getElementById("ship5").style.left = "0%";
+    document.getElementById("message").style.visibility = "hidden";
+    }
+}
+
+
+function loop()
+{
+    if (running)
+    {
+        var rand = Math.round(Math.random() * 100 + 100);
+        setTimeout(function ()
+        {
             MoveShips();
             loop();
         }, rand)
@@ -30,26 +55,53 @@ function loop() {
 
 function MoveShips()
 {      
-    if (ship1Left < finish && ship2Left < finish)
+    if (ship1Left < finish && ship2Left < finish && ship3Left < finish && ship4Left < finish && ship5Left < finish)
     {
-            var amt = Math.round(Math.random() * 2) + 1;
-            if (ship1Left + amt <= 95) {
+            var amt = Math.round(Math.random() * 100);
+            if (ship1Left + amt <= finish) {
                 ship1Left += amt;
             }
             else
             {
                 ship1Left = finish;
             }
-            document.getElementById("ship1").style.left = ship1Left.toString() + "%";         
+            document.getElementById("ship1").style.left = ship1Left.toString() + "px";         
    
-            var amt = Math.round(Math.random() * 2) + 1
-            if (ship2Left + amt <= 98) {
+            var amt = Math.round(Math.random() * 100);
+            if (ship2Left + amt <= finish) {
                 ship2Left += amt;
             }
             else {
                 ship2Left = finish;
             }           
-            document.getElementById("ship2").style.left = ship2Left.toString() + "%";          
+            document.getElementById("ship2").style.left = ship2Left.toString() + "px";
+
+            var amt = Math.round(Math.random() * 100);
+            if (ship3Left + amt <= finish) {
+                ship3Left += amt;
+            }
+            else {
+                ship3Left = finish;
+            }
+            document.getElementById("ship3").style.left = ship3Left.toString() + "px";
+
+            var amt = Math.round(Math.random() * 100);
+            if (ship4Left + amt <= finish) {
+                ship4Left += amt;
+            }
+            else {
+                ship4Left = finish;
+            }
+            document.getElementById("ship4").style.left = ship4Left.toString() + "px";
+
+            var amt = Math.round(Math.random() * 100);
+            if (ship5Left + amt <= finish) {
+                ship5Left += amt;
+            }
+            else {
+                ship5Left = finish;
+            }
+            document.getElementById("ship5").style.left = ship5Left.toString() + "px";
 
     } else {
 
@@ -59,11 +111,20 @@ function MoveShips()
 
         if (ship1Left == finish)
         {
-            document.getElementById("message").innerText = "Ship 1 wins!";
+            document.getElementById("message").innerText = "Ship 1 wins! Press here to reset.";
             //ship 1 wins
-        } else {
-            document.getElementById("message").innerText = "Ship 2 wins!";
+        } else if (ship2Left == finish) {
+            document.getElementById("message").innerText = "Ship 2 wins! Press here to reset.";
             //ship 2 wins
+        } else if (ship3Left == finish) {
+            document.getElementById("message").innerText = "Ship 3 wins! Press here to reset.";
+            //ship 3 wins
+        } else if (ship4Left == finish) {
+            document.getElementById("message").innerText = "Ship 4 wins! Press here to reset.";
+            //ship 4 wins
+        } else if (ship5Left == finish) {
+            document.getElementById("message").innerText = "Ship 5 wins! Press here to reset.";
+            //ship 5 wins
         }
 
         
