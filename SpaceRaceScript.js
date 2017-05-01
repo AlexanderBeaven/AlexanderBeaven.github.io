b033d244b11ce352a3ac0b510f6fa4da71291ad8
AlexanderBeaven <Beaven1@live.missouristate.edu>
2017-05-01 18:01:56 -0500

    v013
    ships light up
    more variation in speed

--- SpaceRaceScript.js
+++ SpaceRaceScript.js
@@ -8,20 +8,24 @@ var ship1Amt = 0;
 var ship2Amt = 0;
 var ship3Amt = 0;
 var ship4Amt = 0;
+var rand = 0;
 
-var finish = screen.width - 68;
-var speed = screen.width / 6;
+var finish = screen.width - 100;
+var speed = screen.width / 12;
 
 function GameStarted()
 {
     if (!running && reset)
     {
-        finish = screen.width - 50;
-        speed = screen.width / 8;
+        finish = screen.width - 100;
         reset = false;
         running = true;
         document.getElementById("message").style.visibility = "hidden";
         document.getElementById("startButton").style.backgroundImage = "url(srimages/GreenLight.png)";
+        document.getElementById("ship1img").src = "srimages/Ship1speed.png";
+        document.getElementById("ship2img").src = "srimages/Ship2speed.png";
+        document.getElementById("ship3img").src = "srimages/Ship3speed.png";
+        document.getElementById("ship4img").src = "srimages/Ship4speed.png";
         loop();
     }
 }
@@ -31,18 +35,16 @@ function Reset()
     if (!reset)
     {
     reset = true;
+    speed = screen.width / 12;
     ship1Left = 0;
     ship2Left = 0;
     ship3Left = 0;
     ship4Left = 0;
-    ship1amt = 0;
-    ship2amt = 0;
-    ship3amt = 0;
-    ship4amt = 0;
-    ship1Rem = 0;
-    ship2Rem = 0;
-    ship3Rem = 0;
-    ship4Rem = 0;
+    ship1Amt = 0;
+    ship2Amt = 0;
+    ship3Amt = 0;
+    ship4Amt = 0;
+    rand = 0;
     document.getElementById("ship1").style.left = "0%";
     document.getElementById("ship2").style.left = "0%";
     document.getElementById("ship3").style.left = "0%";
@@ -56,68 +58,68 @@ function loop()
 {
     if (running)
     {
-        var rand = 490;
+        
         setTimeout(function ()
         {
             MoveShips();
             loop();
-        }, rand)
-    };
+            speed += 10;
+            rand = 495;
+        }, rand);     
+        
+    }
 };
 
 function MoveShips()
-{      
+{
+    //see if any ships have crossed finish line
     if (ship1Left < finish && ship2Left < finish && ship3Left < finish && ship4Left < finish)
     {
-
-        var amt = Math.round(Math.random() * speed - 10);
+        var amt = Math.round(Math.random() * speed);
         if (amt < ship1Amt) //ship can only gain speed
         {
             amt = ship1Amt;
         }
-
         ship1Left += amt;          
         ship1Amt = amt;
         document.getElementById("ship1").style.left = ship1Left.toString() + "px";
 
-        var amt = Math.round(Math.random() * speed - 10);
+        var amt = Math.round(Math.random() * speed);
         if (amt < ship2Amt) //ship can only gain speed
         {
             amt = ship2Amt;
-        }
-       
+        }     
         ship2Left += amt;
         ship2Amt = amt;
         document.getElementById("ship2").style.left = ship2Left.toString() + "px";
 
-        var amt = Math.round(Math.random() * speed - 10);
+        var amt = Math.round(Math.random() * speed);
         if (amt < ship3Amt) //ship can only gain speed
         {
             amt = ship3Amt;
-        }
-       
+        }      
         ship3Left += amt;
         ship3Amt = amt;
         document.getElementById("ship3").style.left = ship3Left.toString() + "px";
 
-        var amt = Math.round(Math.random() * speed - 10);
+        var amt = Math.round(Math.random() * speed);
         if (amt < ship4Amt) //ship can only gain speed
         {
             amt = ship4Amt;
-        }
-       
+        }      
         ship4Left += amt;
         ship4Amt = amt;
         document.getElementById("ship4").style.left = ship4Left.toString() + "px";
-
-
     }
     else
     {
-
         running = false;
         document.getElementById("startButton").style.backgroundImage = "url(srimages/RedLight.png)";
         document.getElementById("message").style.visibility = "visible";
+        document.getElementById("ship1img").src = "srimages/Ship1.png";
+        document.getElementById("ship2img").src = "srimages/Ship2.png";
+        document.getElementById("ship3img").src = "srimages/Ship3.png";
+        document.getElementById("ship4img").src = "srimages/Ship4.png";
 
         var winner = Math.max(ship1Left, ship2Left, ship3Left, ship4Left); //determine which ship would have gone further had it not reached the end. that ship is winner.
 
