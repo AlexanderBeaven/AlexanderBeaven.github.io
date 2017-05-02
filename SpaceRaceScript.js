4d5c8577b351dbecb9b7b7816209543346b53820
AlexanderBeaven <Beaven1@live.missouristate.edu>
2017-05-01 21:10:23 -0500

    v017

--- SpaceRaceScript.js
+++ SpaceRaceScript.js
@@ -14,8 +14,8 @@ var background1Scroll = 300;
 var rand = 0;
 var finish = screen.width - 100;
 var speed = screen.width / 15;
-var accel = 20;
-var buffer = 3;
+var accel = 15;
+var buffer = 2;
 
 function DynamicSizes() //resize and respace ships depending on screen size
 {
@@ -91,14 +91,13 @@ function loop()
 function MoveShips()
 {
     //see if any ships have crossed finish line
-    if (ship1Left < finish && ship2Left < finish && ship3Left < finish && ship4Left < finish)
-    {
+    if (ship1Left < finish && ship2Left < finish && ship3Left < finish && ship4Left < finish) {
         var amt = Math.round(Math.random() * speed + buffer);
         if (amt < ship1Amt) //ship can only gain speed
         {
             amt = ship1Amt;
         }
-        ship1Left += amt;          
+        ship1Left += amt;
         ship1Amt = amt;
         document.getElementById("ship1").style.left = ship1Left.toString() + "px";
         document.getElementById("ship1").style.right = (document.getElementById("ship1").style.right - amt).toString() + "px";
@@ -107,7 +106,7 @@ function MoveShips()
         if (amt < ship2Amt) //ship can only gain speed
         {
             amt = ship2Amt;
-        }     
+        }
         ship2Left += amt;
         ship2Amt = amt;
         document.getElementById("ship2").style.left = ship2Left.toString() + "px";
@@ -117,7 +116,7 @@ function MoveShips()
         if (amt < ship3Amt) //ship can only gain speed
         {
             amt = ship3Amt;
-        }      
+        }
         ship3Left += amt;
         ship3Amt = amt;
         document.getElementById("ship3").style.left = ship3Left.toString() + "px";
@@ -127,16 +126,17 @@ function MoveShips()
         if (amt < ship4Amt) //ship can only gain speed
         {
             amt = ship4Amt;
-        }      
+        }
         ship4Left += amt;
         ship4Amt = amt;
         document.getElementById("ship4").style.left = ship4Left.toString() + "px";
         document.getElementById("ship4").style.right = (document.getElementById("ship4").style.right - amt).toString() + "px";
 
-
+        //subtract minimum amount from scroll time of backgrounds
+       
     }
-    else
-    {
+
+    else {
         running = false;
         document.getElementById("startButton").style.backgroundImage = "url(srimages/RedLight.png)";
         document.getElementById("message").style.visibility = "visible";
@@ -147,18 +147,18 @@ function MoveShips()
 
         var winner = Math.max(ship1Left, ship2Left, ship3Left, ship4Left); //determine which ship would have gone further had it not reached the end. that ship is winner.
 
-           if (ship1Left == winner) {
-               document.getElementById("message").innerText = "Ship 1 wins! Press here to reset.";
-               //ship 1 wins
-           } else if (ship2Left == winner) {
-               document.getElementById("message").innerText = "Ship 2 wins! Press here to reset.";
-               //ship 2 wins
-           } else if (ship3Left == winner) {
-               document.getElementById("message").innerText = "Ship 3 wins! Press here to reset.";
-               //ship 3 wins
-           } else if (ship4Left == winner) {
-               document.getElementById("message").innerText = "Ship 4 wins! Press here to reset.";
-               //ship 4 wins       
-           }
+        if (ship1Left == winner) {
+            document.getElementById("message").innerText = "Ship 1 wins! Press here to reset.";
+            //ship 1 wins
+        } else if (ship2Left == winner) {
+            document.getElementById("message").innerText = "Ship 2 wins! Press here to reset.";
+            //ship 2 wins
+        } else if (ship3Left == winner) {
+            document.getElementById("message").innerText = "Ship 3 wins! Press here to reset.";
+            //ship 3 wins
+        } else if (ship4Left == winner) {
+            document.getElementById("message").innerText = "Ship 4 wins! Press here to reset.";
+            //ship 4 wins       
+        }
     }
 }
